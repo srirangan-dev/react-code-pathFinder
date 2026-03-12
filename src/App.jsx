f0939143d93efc3034d6f392f3e3c9f3b9d91f4c
Sri Rangan <srirangan0603@gmail.com>
2026-03-12 21:59:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Login             from './pages/Login'
 import Signup            from './pages/Signup'
 import LearnSection      from './components/LearnSection'  // ✅ new
 
+
 export default function App() {
   return (
     <AuthProvider>
